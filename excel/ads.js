c5ad1c1a8c47e420fa9532ecb1d3fee299685f74
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-11-21 12:14:04 -0300

    facebook

---
+++ excel/ads.js
@@ -0,0 +1,85 @@
+export const ads = [
+	{
+		"name": "No encontró id=173245975874895",
+		"text": "",
+		"images": []
+	},
+	{
+		"name": "Ciudad Moto",
+		"text": "¡Haz realidad tu sueño de tener una moto nueva! 🏍️💨\nCon nuestro crédito personal, solo con tu DNI podrás retirar tu moto ¡el mismo día! 😱\n\n✔️ Cuotas fijas y accesibles\n✔️ Sin necesidad de anticipo\n✔️ El único requisito es ser mayor de 18 años\n\n📌¡Contáctanos al 112192-7189!\n📍 Visítanos en cualquiera de nuestras 9 sucursales ubicadas en GBA SUR, GBA NORTE, La Plata, y CABA.",
+		"images": [
+			{
+				"body": " ",
+				"caption": "api.whatsapp.com",
+				"ctaText": "Send WhatsApp Message",
+				"ctaType": "WHATSAPP_MESSAGE",
+				"imageCrops": [],
+				"linkDescription": " ",
+				"linkUrl": "https://api.whatsapp.com/send?phone=5491121927189",
+				"originalImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/465783712_1750598332011417_2463751227340379328_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=xLkuIpXhtuwQ7kNvgGIUGeu&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYAdtVdr-YpM9BNZr5_JKm_-Mt6QmxA9ENVeKWOvJCrLBg&oe=6745027E",
+				"resizedImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/465763257_1305776963791248_2431215642677483579_n.jpg?stp=dst-jpg_s600x600&_nc_cat=102&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=jsFnFpZ_VNAQ7kNvgHhVXsa&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYAAjjkij6AYxFpWh-4bo00E4SEE_ulfBjMeMpkiNtC7Zw&oe=67450B54",
+				"watermarkedResizedImageUrl": "",
+				"title": "¡Llévate tu nueva moto!",
+				"videoHdUrl": null,
+				"videoPreviewImageUrl": null,
+				"videoSdUrl": null,
+				"watermarkedVideoHdUrl": null,
+				"watermarkedVideoSdUrl": null
+			},
+			{
+				"body": " ",
+				"caption": "api.whatsapp.com",
+				"ctaText": "Send WhatsApp Message",
+				"ctaType": "WHATSAPP_MESSAGE",
+				"imageCrops": [],
+				"linkDescription": " ",
+				"linkUrl": "https://api.whatsapp.com/send?phone=5491121927189",
+				"originalImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/466091301_1582529969316015_1558771060947882968_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=Y4544_C_mwQQ7kNvgFSU72E&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYBDzy-hcMvYMJ4pmQN5pMvv4e18dYcqu_ZMli8hnkkjsw&oe=67452114",
+				"resizedImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/465789186_1233574421031307_4149639985351252858_n.jpg?stp=dst-jpg_s600x600&_nc_cat=104&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=aJ4vjHdUn2wQ7kNvgF8Q7MB&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYDn0eGiT79HfZd0QnHW4tBTiYKMAwP-ZgvDuPRx4PH2jQ&oe=67452948",
+				"watermarkedResizedImageUrl": "",
+				"title": "Solo con DNI",
+				"videoHdUrl": null,
+				"videoPreviewImageUrl": null,
+				"videoSdUrl": null,
+				"watermarkedVideoHdUrl": null,
+				"watermarkedVideoSdUrl": null
+			},
+			{
+				"body": " ",
+				"caption": "api.whatsapp.com",
+				"ctaText": "Send WhatsApp Message",
+				"ctaType": "WHATSAPP_MESSAGE",
+				"imageCrops": [],
+				"linkDescription": " ",
+				"linkUrl": "https://api.whatsapp.com/send?phone=5491121927189",
+				"originalImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/466424429_1799664584115890_6271982509880725627_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=clonNz2BPxIQ7kNvgF7nlba&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYDtBC_j-1Bv94XMNWC5aZajvHQjJ7Cb8NoZ56jYH6kUnw&oe=67450DF3",
+				"resizedImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/466090071_1115518080296373_6242009460253813009_n.jpg?stp=dst-jpg_s600x600&_nc_cat=103&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=8f133sI62O8Q7kNvgEKrFgQ&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYDlMhyKHSgWTPwYPHi8unp8MIxyRQp4lINQ8aq9ZZqbYQ&oe=67452E9C",
+				"watermarkedResizedImageUrl": "",
+				"title": "SIN ANTICIPO",
+				"videoHdUrl": null,
+				"videoPreviewImageUrl": null,
+				"videoSdUrl": null,
+				"watermarkedVideoHdUrl": null,
+				"watermarkedVideoSdUrl": null
+			},
+			{
+				"body": " ",
+				"caption": "api.whatsapp.com",
+				"ctaText": "Send WhatsApp Message",
+				"ctaType": "WHATSAPP_MESSAGE",
+				"imageCrops": [],
+				"linkDescription": " ",
+				"linkUrl": "https://api.whatsapp.com/send?phone=5491121927189",
+				"originalImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/466088764_619206520447368_1562860708119725100_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=CAPe3wJSH9IQ7kNvgE3L92A&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYDu_L2i6s_UsUk7fgfwLlxS2oNbf8TeHUHOzUlEyy_mQA&oe=67452F87",
+				"resizedImageUrl": "https://scontent.fcau32-1.fna.fbcdn.net/v/t39.35426-6/465735416_1757108098371063_7272553356051465361_n.jpg?stp=dst-jpg_s600x600&_nc_cat=103&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=W_csOcYB278Q7kNvgHKHK5m&_nc_zt=14&_nc_ht=scontent.fcau32-1.fna&_nc_gid=Amy_m1LzZqyVvT3j6QgcnHF&oh=00_AYA7s2dauhFgFfrBCplZz830V3AiGNYlLO66NWpDw5vVsw&oe=674519F0",
+				"watermarkedResizedImageUrl": "",
+				"title": "Entrega inmediata de tu 0KM",
+				"videoHdUrl": null,
+				"videoPreviewImageUrl": null,
+				"videoSdUrl": null,
+				"watermarkedVideoHdUrl": null,
+				"watermarkedVideoSdUrl": null
+			}
+		]
+	}
+]
--- functions/scrapeFacebook.js
+++ functions/scrapeFacebook.js
@@ -1,13 +1,111 @@
-import axios from "axios"
+import axios from "axios";
+import ExcelJS from "exceljs";
+import { ads } from "../excel/ads.js";
+import path from "path";
 
-export const scrapeFacebook = async(userPhone)=>{
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    try {
-        // Uses other API as a microservice for scrapping
-        const precios = await axios.get("https://three-2-13-web-scrapping.onrender.com/scrape/facebook");
-        console.log("Precios:", precios.data)
-       
-    } catch (error) {
-        console.log("Error in scrapeFacebook.js:", error.message)
-    }
-}
+export const scrapeFacebook = async (userPhone) => {
+	try {
+		// Uses other API as a microservice for scrapping
+		//const ads = await axios.get("https://three-2-13-web-scrapping.onrender.com/scrape/facebook");
+		//console.log("Precios:", ads.data)
+
+		//const results = ads.data
+		const results = ads;
+		// Crear un nuevo libro de Excel
+		const workbook = new ExcelJS.Workbook();
+
+		// Inicializar la fila donde comenzaremos a agregar resultados
+		let startRow = 1;
+
+		// Iterar sobre los nombres agrupados
+		for (const ad of results) {
+			const name = ad.name; // Obtener el nombre del anuncio
+			const group = [ad]; // Agrupar el anuncio en un array
+
+			// Crear una nueva hoja con el nombre del anunciante
+			const worksheet = workbook.addWorksheet(name);
+
+			// Inicializar la fila para este grupo
+			startRow = 1;
+
+			for (const result of group) {
+				const { text, images } = result;
+
+				// Agregar el texto a la celda correspondiente
+				worksheet.getCell(`A${startRow}`).value = text;
+
+				// Contar la cantidad de imágenes y cards
+				const imageCount = images ? images.length : 0;
+
+				// Agregar la cantidad de avisos debajo del texto
+				worksheet.getCell(
+					`A${startRow + 1}`
+				).value = `Cantidad de avisos: ${imageCount}`;
+
+				// Ajustar el tamaño de las celdas para las imágenes
+				worksheet.getColumn(1).width = 40;
+				worksheet.getColumn(2).width = 5;
+				worksheet.getColumn(3).width = 40;
+				worksheet.getColumn(4).width = 5;
+				worksheet.getColumn(5).width = 40;
+
+				// Agregar imágenes de "images"
+				if (imageCount > 0) {
+					for (let i = 0; i < imageCount; i++) {
+						const imageUrl = images[i].originalImageUrl;
+
+						// Descargar la imagen y agregarla a la hoja
+						const response = await fetch(imageUrl);
+						if (!response.ok)
+							throw new Error(`Error al descargar la imagen: ${imageUrl}`);
+						const arrayBuffer = await response.arrayBuffer();
+						const buffer = Buffer.from(arrayBuffer); // Convertir a Buffer
+						const imageId = workbook.addImage({
+							buffer,
+							type: "picture",
+							extension: "png",
+						});
+
+						// Calcular la fila y columna para la imagen
+						const row = startRow + 1;
+						const col = (i * 2) % 6; // Columna 0, 2, 4 para las imágenes
+
+						// Agregar la imagen a la celda correspondiente
+						worksheet.addImage(imageId, {
+							tl: { col: col, row: row },
+							br: { col: col + 1, row: row + 1 },
+							editAs: "oneCell",
+						});
+
+						// Ajustar la altura de la fila para que se muestre la imagen
+						worksheet.getRow(row + 1).height = 400;
+
+						// Incrementar la fila después de cada 3 imágenes
+						if ((i + 1) % 3 === 0) {
+							startRow += 2;
+						}
+						// Ajustar la altura de la fila vacía debajo de las imágenes
+						worksheet.getRow(row + 2).height = 15;
+					}
+				}
+
+				// Incrementar la fila de inicio para el siguiente aviso
+				startRow += Math.ceil(imageCount / 3) * 2;
+			}
+		}
+
+		// Define a temporal file for the excel
+		const tempFilePath = path.join(__dirname, "../public/Avisos Facebook.xlsx");
+		
+		// Guardar el archivo de Excel
+		await workbook.xlsx.writeFile(tempFilePath);
+		console.log(`Archivo de Excel creado en: ${tempFilePath}`);
+		//return facebookAds
+	} catch (error) {
+		console.log("Error in scrapeFacebook.js:", error.message);
+	}
+};
+
