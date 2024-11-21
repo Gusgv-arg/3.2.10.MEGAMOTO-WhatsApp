892c9241a32c8ad13e896d72cf50ebf3807adad7
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-11-21 10:23:59 -0300

    .data

--- functions/scrapeFacebook.js
+++ functions/scrapeFacebook.js
@@ -5,9 +5,9 @@ export const scrapeFacebook = async(userPhone)=>{
     try {
         // Uses other API as a microservice for scrapping
         const precios = await axios.get("https://three-2-13-web-scrapping.onrender.com/scrape/facebook");
-        console.log("Precios:", precios)
+        console.log("Precios:", precios.data)
        
     } catch (error) {
-        console.log("error in scrapeFacebook.js:", error.message)
+        console.log("Error in scrapeFacebook.js:", error.message)
     }
 }
