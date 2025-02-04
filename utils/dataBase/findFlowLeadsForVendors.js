fe4440d18ee3ab2ff89db7315137858bcf7618b3
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-02-04 15:16:22 -0300

    fecha

--- utils/dataBase/findFlowLeadsForVendors.js
+++ utils/dataBase/findFlowLeadsForVendors.js
@@ -4,9 +4,10 @@ import Leads from "../../models/leads.js";
 export const findFlowLeadsForVendors = async () => {
 	const currentDate = new Date();
 	const twentyFourHoursAgo = new Date(currentDate - 24 * 60 * 60 * 1000);
-	
-	console.log("ahora:", currentDate)
-	console.log("24hs atras:", twentyFourHoursAgo)
+
+	console.log("ahora:", currentDate);
+	console.log("24hs atras:", twentyFourHoursAgo);
+	console.log("24hs atrás (ISO):", twentyFourHoursAgo.toISOString());
 
 	const leads = await Leads.find({
 		$or: [
@@ -46,49 +47,63 @@ export const findFlowLeadsForVendors = async () => {
 							],
 						},
 					},
-					{ "flows.flowDate": { $lte: twentyFourHoursAgo.toISOString() } },
+					{
+						$expr: {
+							$lt: [
+								{
+									$dateFromString: {
+										dateString: "$flows.flowDate",
+										format: "%d/%m/%Y, %I:%M:%S %p",
+									},
+								},
+								twentyFourHoursAgo,
+							],
+						},
+					},
 				],
 			},
 		],
 	}).lean();
 
-	return leads
-		.filter((lead) => lead.botSwitch !== "OFF")
-		/* .filter((lead) => {
+	return (
+		leads
+			.filter((lead) => lead.botSwitch !== "OFF")
+			/* .filter((lead) => {
 			const lastFlow = lead.flows[lead.flows.length - 1];
 			return !lastFlow || !lastFlow.vendor_phone || lastFlow.vendor_phone !== "";
 		}) */
-		.map((lead) => {
-			const lastFlow =
-				lead.flows.length > 0 ? lead.flows[lead.flows.length - 1] : null;
+			.map((lead) => {
+				const lastFlow =
+					lead.flows.length > 0 ? lead.flows[lead.flows.length - 1] : null;
 
-			return {
-				name: lead.name,
-				id_user: lead.id_user,
-				botSwitch: lead.botSwitch,
-				lastFlow: lastFlow
-					? {
-							flowName: lastFlow.flowName,
-							flowDate: lastFlow.flowDate,
-							client_status: lastFlow.client_status,
-							toContact: lastFlow.toContact,
-							brand: lastFlow.brand,
-							model: lastFlow.model,
-							price: lastFlow.price,
-							otherProducts: lastFlow.otherProducts,
-							payment: lastFlow.payment,
-							dni: lastFlow.dni,
-							questions: lastFlow.questions,
-							messages: lastFlow.messages,
-							vendor_name: lastFlow.vendor_name,
-							vendor_phone: lastFlow.vendor_phone,
-							history: lastFlow.history,
-							flow_2token: lastFlow.flow_2token,
-							flow_status: lastFlow.flow_status,
-							error: lastFlow.error,
-							origin: lastFlow.origin
-					  }
-					: null,
-			};
-		});
+				return {
+					name: lead.name,
+					id_user: lead.id_user,
+					botSwitch: lead.botSwitch,
+					lastFlow: lastFlow
+						? {
+								flowName: lastFlow.flowName,
+								flowDate: lastFlow.flowDate,
+								client_status: lastFlow.client_status,
+								toContact: lastFlow.toContact,
+								brand: lastFlow.brand,
+								model: lastFlow.model,
+								price: lastFlow.price,
+								otherProducts: lastFlow.otherProducts,
+								payment: lastFlow.payment,
+								dni: lastFlow.dni,
+								questions: lastFlow.questions,
+								messages: lastFlow.messages,
+								vendor_name: lastFlow.vendor_name,
+								vendor_phone: lastFlow.vendor_phone,
+								history: lastFlow.history,
+								flow_2token: lastFlow.flow_2token,
+								flow_status: lastFlow.flow_status,
+								error: lastFlow.error,
+								origin: lastFlow.origin,
+						  }
+						: null,
+				};
+			})
+	);
 };
