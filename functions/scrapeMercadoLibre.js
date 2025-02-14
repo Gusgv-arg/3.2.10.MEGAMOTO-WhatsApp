cc6472c7c1dc4d4c23377bfba7400f2908e65096
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-02-14 15:35:45 -0300

    mod

--- functions/scrapeMercadoLibre.js
+++ functions/scrapeMercadoLibre.js
@@ -83,8 +83,10 @@ export const scrapeMercadoLibre = async (userPhone) => {
 				responseType: "arraybuffer",
 			});
 			console.log("response.data:", response.data)
+			const arrayBuffer = response.data.buffer.slice(response.data.byteOffset, response.data.byteOffset + response.data.byteLength);
+			await workbook.xlsx.load(arrayBuffer);
 			
-			await workbook.xlsx.load(response.data);
+			//await workbook.xlsx.load(response.data);
 			//console.log("Template file loaded successfully");
 		} catch (error) {
 			console.log("Error al acceder a precios_template.xlsx", error.message);
@@ -95,6 +97,7 @@ export const scrapeMercadoLibre = async (userPhone) => {
 
 		// Seleccionar la hoja "Avisos"
 		const avisosSheet = workbook.getWorksheet("Avisos");
+		console.log("Contenido de la hoja 'Avisos':", avisosSheet.getSheetValues());
 
 		if (!avisosSheet) {
 			throw new Error("La hoja 'Avisos' no existe en el archivo predefinido.");
