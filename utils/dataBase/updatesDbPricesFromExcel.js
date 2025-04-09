7774aa44ebb07fbca87ca2bbfc10b9a21dc04714
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-04-09 17:07:35 -0300

    desactivar

--- utils/dataBase/updatesDbPricesFromExcel.js
+++ utils/dataBase/updatesDbPricesFromExcel.js
@@ -116,11 +116,12 @@ export const updateDbPricesFromExcel = async () => {
 
 		// Crear una lista de modelos presentes en el Excel
 		let modelosEnExcel = dataExcel.map((entrada) => entrada.B.trim().toLowerCase());
+		console.log("Modelos en Excel (normalizados):", modelosEnExcel);
 
 		// Busca y cambia isActive a false a los registros que están en la base pero no en el Excel
 		try {
-			await Prices.updateMany(
-				{ modelo: { $nin: modelosEnExcel.map((modelo) => modelo.trim().toLowerCase()) } },
+			const result = await Prices.updateMany(
+				{ modelo: { $nin: modelosEnExcel } },
 				{ $set: { isActive: false } }
 			);
 		} catch (error) {
